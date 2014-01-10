f2e33ed2dfd310e1af058e3197b9a27430283bbd
Arun Babu Neelicattu <abn@redhat.com>
2014-01-10 22:19:53 +1000

    Service v1 EOL: Retire tests

--- test/service_v1.py
+++ test/service_v1.py
@@ -24,60 +24,10 @@ from test import FlaskTestCase
 
 
 class TestServiceV1(FlaskTestCase):
+
     """
     Tests for version 1 of the web service.
     """
-
-    points = ['update', 'remove']
-
-    def test_uri_endpoints(self):
-        """
-        Verify the basic endpoints respond as they should.
-        """
-        for kind in self.points:
-            resp = self.app.get('/service/v1/%s/0/' % kind)
-            assert resp.status_code == 200
-            assert resp.content_type == 'application/json'
-
-        # V1 returns empty list when nothing is available
-        for kind in self.points:
-            resp = self.app.get('/service/v1/%s/999999/' % kind)
-            assert resp.status_code == 200
-            assert resp.content_type == 'application/json'
-
-        # Anything that is not an int should be a 404
-        for kind in self.points:
-            for badtype in ['NotAnInt', 10.436, u'bleh']:
-                resp = self.app.get('/service/v1/%s/%s/' % (kind, badtype))
-                print(resp.status_code, kind, resp)
-                assert resp.status_code == 400
-                assert resp.content_type == 'application/json'
-
-    def test_data_structure(self):
-        """
-        Ensures the response structure is correct.
-        """
-        resp = self.app.get('/service/v1/update/0/')
-        result = json.loads(resp.data)
-
-        assert len(result) > 0
-
-        expected = {
-            'status': basestring,
-            'db_version': int,
-            'vendor': basestring,
-            'name': basestring,
-            'format': basestring,
-            'version': basestring,
-            'submitter': basestring,
-            'hash': basestring,
-            'cves': basestring,
-        }
-
-        for item in result:
-            assert 'fields' in item.keys()
-            for key, testtype in expected.items():
-                assert isinstance(item['fields'][key], testtype)
 
     def test_status(self):
         """
@@ -88,11 +38,11 @@ class TestServiceV1(FlaskTestCase):
 
         result = json.loads(resp.data)
 
-        import datetime
-        from victims_web.blueprints.service_v1 import EOL
+        from datetime import datetime
+        from victims_web.blueprints.service.v1 import EOL
 
         assert result['version'] == '1'
         assert result['recommended'] is False
         assert result['eol'] == EOL.isoformat()
-        assert result['supported'] == (datetime.datetime.now() <= EOL)
+        assert result['supported'] == (datetime.now() <= EOL)
         assert result['endpoint'] == '/service/v1/'
