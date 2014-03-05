74ba64679a4817d96766c5e1bacce38ef02b19ed
Arun Babu Neelicattu <abn@redhat.com>
2014-03-05 23:53:20 +1000

    Changes to shell.sh

--- contrib/scripts/shell.sh
+++ contrib/scripts/shell.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-python -c \
+PYTHON_CMD="python"
+
+${PYTHON_CMD} -c \
     "from flask.ext.script import Manager; \
     from victims_web.application import app; \
     Manager(app).run()" \
